4dd75f9a60ed85b6d39bb1658b6d4896f3def133
IZY-4 <yura.zhinchyn@gmail>
2024-11-06 11:22:44 +0200

    changed class variable name, used multiple f-string in str and repr, renamed variables in main func

--- src/lab4.py
+++ src/lab4.py
@@ -1,14 +1,14 @@
 class Insect:
 
-    legs = 6
+    number_of_legs = 6
 
-    def __init__(self, name = None, speed = None, mass = None, population = None, habitat = None, legs= None):
+    def __init__(self,name=None,speed=None,mass=None,population=None,habitat=None,legs=None):
         self.__name = name
         self.__speed = speed
         self.__mass = mass
         self.population = population
         self.habitat = habitat
-        self.legs = legs
+        self.legs = legs if legs is not None else Insect.number_of_legs
 
     def get_name(self):
         return self.__name
@@ -29,27 +29,38 @@ class Insect:
         self.__mass = mass
 
     def __str__(self):
-        return f"Insect: {self.__name}, {self.__speed} m/s, {self.__mass} g, {self.legs} Population: {self.population}, Habitat: {self.habitat}"
+        return (
+            f"Insect:\n"
+            f"  Name: {self.__name}\n"
+            f"  Speed: {self.__speed} m/s\n"
+            f"  Mass: {self.__mass} g\n"
+            f"  Legs: {self.legs}\n"
+            f"  Population: {self.population}\n"
+            f"  Habitat: {self.habitat}"
+        )
 
     def __repr__(self):
-        return f"Insect(name={self.__name}, speed={self.__speed}, mass={self.__mass}, legs = {self.legs}, population = {self.population}, habitat = {self.habitat})"
+        return (
+            f"Insect(name={self.__name}, speed={self.__speed}, mass={self.__mass}, "
+            f"legs={self.legs}, population={self.population}, habitat={self.habitat})"
+        )
     
     def __del__(self):
         print(f"Object '{self.__name}' was deleted")
 
 def main():
-    insect1 = Insect("Bee", 5.5, 0.1, 1500, "Tropics")
-    insect2 = Insect("Butterfly", 2.0, 0.3, 2000, "Forest")
-    insect3 = Insect("Ant", 0.02, 0.002, 2500, "Anywhere")
-    insect4 = Insect()
-    insect5 = Insect("1", 1, 1, 1, "1", 10)
+    insect_1 = Insect("Bee", 5.5, 0.1, 1500, "Tropics")
+    insect_2 = Insect("Butterfly", 2.0, 0.3, 2000, "Forest")
+    insect_3 = Insect("Ant", 0.02, 0.002, 2500, "Anywhere")
+    insect_4 = Insect()
+    insect_5 = Insect("Insect 5", 1, 1, 1, "Habitat 5", 10)
 
-    print(insect1)
-    print(insect2)
-    print(insect3)
-    print(insect4)
-    print(Insect.legs)
-    print(insect5)
-    print(insect5.legs)
+    print(insect_1)
+    print(insect_2)
+    print(insect_3)
+    print(insect_4)
+    print(Insect.number_of_legs)
+    print(insect_5)
+    print(insect_5.legs)
 
 main()
